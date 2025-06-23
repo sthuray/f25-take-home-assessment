e3972bb19e45c6b6c592516ce8b5a2f1f865d9aa
sthuray <138075787+sthuray@users.noreply.github.com>
2025-06-22 23:49:23 -0400

    Implement POST /weather

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, Any, Optional
 import uvicorn
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+WEATHERSTACK_API_KEY=os.getenv('WEATHERSTACK_API_KEY')
 
 app = FastAPI(title="Weather Data System", version="1.0.0")
 
@@ -34,7 +40,22 @@ async def create_weather_request(request: WeatherRequest):
     3. Stores combined data with unique ID in memory
     4. Returns the ID to frontend
     """
-    pass
+    weather_id = f"{request.location.replace(" ", "-")}-{request.date}"
+    weatherResponse=WeatherResponse(id=weather_id)
+    if weather_id in weather_storage:
+        raise HTTPException(status_code=400, detail=f"Data already exists with date {request.date} and location {request.location}")
+    
+    url = f"http://api.weatherstack.com/current?access_key={WEATHERSTACK_API_KEY}"
+    querystring = {"query":request.location}
+    response = requests.get(url, params=querystring)
+    data = response.json()
+    if "error" in data:
+        raise HTTPException(status_code=data["error"]["code"], detail=f"{data["error"]["type"]}: {data["error"]["info"]}")
+
+    weather_storage[weather_id] = data
+
+    return weatherResponse
+    
 
 @app.get("/weather/{weather_id}")
 async def get_weather_data(weather_id: str):
